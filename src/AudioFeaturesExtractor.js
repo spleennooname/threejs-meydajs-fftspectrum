28b36c3cfd6f21fd677f74e78ef04b25129c790d
Andrea Bovo <spleen666@gmail.com>
2022-01-13 20:42:35 +0100

    update extractor, deps, refactoring

--- src/AudioFeaturesExtractor.js
+++ src/AudioFeaturesExtractor.js
@@ -1,8 +1,11 @@
 
 import * as Meyda from "meyda";
-import { tap, catchError, Observable } from "rxjs";
 
-const AUDIO_CONSTRAINS = {
+navigator.getUserMedia = navigator.webkitGetUserMedia ||
+  navigator.getUserMedia ||
+  navigator.mediaDevices.getUserMedia
+
+export const AUDIO_CONSTRAINS = {
   audio: {
     channelCount: {
       ideal: 2,
@@ -11,70 +14,54 @@ const AUDIO_CONSTRAINS = {
     echoCancellation: true,
     autoGainControl: true,
     noiseSuppression: true
-  }
+  } 
 }
-export default class AudioFeaturesExtractor {
 
-  constructor({ bufferSize = 512 }) {
-    this.bufferSize = bufferSize
-    navigator.getUserMedia = navigator.webkitGetUserMedia ||
-      navigator.getUserMedia ||
-      navigator.mediaDevices.getUserMedia;
+export const VIDEO_CONSTRAINS = {
+  video: { facingMode: 'user' } 
+}
+export class AudioFeaturesExtractor {
+
+  meyda$({constrains = AUDIO_CONSTRAINS, bufferSize = 512}) {
+    return navigator
+      .mediaDevices
+      .getUserMedia(constrains)
+      .then(stream => this.successStream({stream, bufferSize}))
+      .catch(err => this.errorStream(err)
+    )
   }
 
-  start(AUDIO_COSTRAINS) {
-    //
-    this.context = new AudioContext()
-    //
-    return this.getUserMedia()
-      .pipe(
-        tap( stream => this.successStream(stream)),
-        catchError(err => this.errorStream(err))
-      )
-  }
+  successStream({stream, bufferSize}) {
 
-  successStream(stream ) {
-    window.persistAudioStream = stream;
-    this.streamSettings = stream.getAudioTracks()[0].getSettings()
-    this.source = this.context.createMediaStreamSource(stream)
-    //
+    this.audioContext = new AudioContext()
+
+    const audioTrack = stream.getAudioTracks()[0]
+
+    if (audioTrack) {
+      this.streamAudioSettings = audioTrack.getSettings()
+    }
+ 
+    this.source = this.audioContext.createMediaStreamSource(stream)
     this.meyda = Meyda.createMeydaAnalyzer({
-      audioContext: this.context,
+      audioContext: this.audioContext,
       source: this.source,
-      sampleRate: this.streamSettings.sampleRate || 44100,
+      //sampleRate: this.streamAudioSettings.sampleRate || 44100,
       // Buffer Size tells Meyda how often to check the audio feature, and is
       // measured in Audio Samples. Usually there are 44100 Audio Samples in 1
       // second, which means in this case Meyda will calculate the level about 43
       // (44100/1024) times per second.
-      bufferSize: this.bufferSize,
-      windowingFunction: "blackman"
+      bufferSize,
+      //windowingFunction: "blackman"
     });
     this.meyda.start()
-    console.log('AudioFeaturesExtractor', stream)
-    return this.meyda
+    return stream
   }
 
-  errorStream(stream){
-    console.error('AudioFeaturesExtractor', stream)
+  errorStream(stream) {
+    console.error("AudioFeaturesExtractor", stream)
   }
 
-  getUserMedia() {
-    return new Observable(subscriber => {
-      navigator
-        .mediaDevices
-        .getUserMedia(AUDIO_CONSTRAINS)
-        .then(mediaStream => {
-          subscriber.next(mediaStream)
-          subscriber.complete()
-        })
-        .catch(err => {
-          subscriber.error(err);
-        })
-        return () => subscriber.complete()
-    })
-  }
-
-  features(features = [
+  features(audioFeatures = [
     "amplitudeSpectrum",
     "spectralCentroid",
     "spectralRolloff",
@@ -84,11 +71,12 @@ export default class AudioFeaturesExtractor {
     "chroma",
     "mfcc",
   ]) {
-    if(this.context) this.context.resume();
-    return this.meyda.get(features)
+    if( !this.meyda || !this.audioContext ) return null
+    return this.meyda.get(audioFeatures)
   }
 
   signal() {
+    if( !this.meyda) return null
     return this.meyda._m.signal;
   }
 }
--- src/index.js
+++ src/index.js
@@ -19,10 +19,9 @@ import {
 import {
   Scene,
   PerspectiveCamera,
-  LineBasicMaterial,
   WebGLRenderer,
   DirectionalLight,
-  ArrowHelper,
+  //ArrowHelper,
   Group,
   Object3D,
   Mesh,
@@ -31,13 +30,13 @@ import {
   BufferAttribute,
   InstancedMesh,
   Vector2,
-  Vector3,
+  //Vector3,
   RawShaderMaterial,
   DynamicDrawUsage,
-  sRGBEncoding,
+  //sRGBEncoding,
   DoubleSide,
   BoxBufferGeometry,
-  MeshBasicMaterial,
+  //MeshBasicMaterial,
   MeshPhongMaterial
 } from "three"
 
@@ -50,12 +49,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { getFFTs, getPalette, lerp, invlerp } from "./utils"
 //
 import { fs, vs } from "./materials/line"
-//
-import AudioFeaturesExtractor from "./AudioFeaturesExtractor"
 
+import {
+  AudioFeaturesExtractor,
+} from "./AudioFeaturesExtractor"
 
 const bufferSize = 512;
-const numLines = 40;
+const numLines = 50;
+
 const aspectRatio = 4 / 3;
 
 // check https://github.com/bpostlethwaite/colormap
@@ -69,13 +70,9 @@ let imeshSignal,
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
-const pixelRatio = Math.min(window.devicePixelRatio, 1.5)
+const pixelRatio = Math.min(1.0, window.devicePixelRatio || 1)
 
 const scene = new Scene();
-
-//const clock = new Clock();
-// start the thing
-const audioFeaturesExtractor = new AudioFeaturesExtractor({ bufferSize });
 
 const camera = new PerspectiveCamera(75, aspectRatio, 5, 100);
 camera.position.set(0, -5, 5);
@@ -94,7 +91,7 @@ let renderer = new WebGLRenderer({
   alpha: false,
   powerPreference: "high-performance"
 });
-renderer.outputEncoding = sRGBEncoding
+//renderer.outputEncoding = sRGBEncoding
 renderer.setPixelRatio(pixelRatio);
 
 const controls = new OrbitControls(camera, canvas);
@@ -111,6 +108,11 @@ controls.minPolarAngle = 1
 controls.maxPolarAngle = Math.PI / 2
 controls.update();
 
+let directionalLight = new DirectionalLight(0xffffff, 0.75);
+directionalLight.position.set(0, 1, 1);
+
+scene.add(directionalLight);
+
 const renderPass = new RenderPass(scene, camera);
 
 const bloomPass = new UnrealBloomPass(resolution, 0, 0, 0);
@@ -121,35 +123,33 @@ bloomPass.radius = 1.5;
 const composer = new EffectComposer(renderer);
 composer.addPass(renderPass);
 composer.addPass(bloomPass);
-bloomPass.renderToScreen = true;
 
-let directionalLight = new DirectionalLight(0xffffff, 0.75);
-directionalLight.position.set(0, 1, 1);
+//bloomPass.renderToScreen = true;
 
 // Unchanging variables
-const length = 1;
-const hex = 0xffff00;
-const dir = new Vector3(0, 1, 0);
+//const length = 1;
+//const hex = 0xffff00;
+//const dir = new Vector3(0, 1, 0);
 
-const rightDir = new Vector3(1, 0, 0);
-const origin = new Vector3(1, -5, -10);
-//
-let centroidArrow = new ArrowHelper(dir, origin, length, 0xffff00);
-let rolloffArrow = new ArrowHelper(dir, origin, length, 0x00ff00);
-let rmsArrow = new ArrowHelper(rightDir, origin, length, 0xff00ff);
+//const rightDir = new Vector3(1, 0, 0);
+//const origin = new Vector3(1, -5, -10);
 
-const lineMaterial = new LineBasicMaterial({ color: 0x00fff0 });
-const yellowMaterial = new LineBasicMaterial({ color: 0x00ffff });
+//let centroidArrow = new ArrowHelper(dir, origin, length, 0xffff00);
+//let rolloffArrow = new ArrowHelper(dir, origin, length, 0x00ff00);
+//let rmsArrow = new ArrowHelper(rightDir, origin, length, 0xff00ff);
+
+//const lineMaterial = new LineBasicMaterial({ color: 0x00fff0 });
+//const yellowMaterial = new LineBasicMaterial({ color: 0x00ffff });
 
 // SETUP FFTS
 
-// init ffts
 let ffts = getFFTs(numLines, bufferSize);
+
 // get meshes color
 let colors = getPalette(paletteLabel, ffts.length)
-let palette = colors.map(c => new Color(c.r, c.g, c.b))
+//let palette = colors.map(c => new Color(c.r, c.g, c.b))
 
-const fftMat = new RawShaderMaterial({
+/* const fftMat = new RawShaderMaterial({
   uniforms: {
     uColor: { value: new Color(1, 1, 1) }
   },
@@ -157,10 +157,10 @@ const fftMat = new RawShaderMaterial({
   fragmentShader: fs,
   transparent: true,
   side: DoubleSide
-});
+}); */
 
 // const fftMatN = new MeshNormalMaterial({ opacity: 0.7 })
-const fftMatL = new MeshBasicMaterial()
+//const fftMatL = new MeshBasicMaterial()
 const fftMatP = new MeshPhongMaterial({ color: 0xffffff })
 
 // 1. INSTANCED MESH for signal
@@ -174,7 +174,6 @@ const signalPalette = getPalette(paletteLabel, bufferSize)
   .map(a => new Color().fromArray(a))
 signalPalette
   .forEach((c, i) => imeshSignal.setColorAt(i, c));
-
 scene.add(imeshSignal);
 
 // 2. group of meshes for fft spectrum
@@ -220,134 +219,14 @@ let bufferLine = new Line(bufferLineGeometry, bufferLineMaterial);
 bufferLineGeometry.setAttribute("position", new BufferAttribute(new Float32Array(bufferSize * 3), 3));
 bufferLineGeometry.setDrawRange(0, bufferSize); */
 
-// add to scene
-scene.add(directionalLight);
-scene.add(centroidArrow);
-scene.add(rolloffArrow);
-scene.add(rmsArrow);
+// scene.add(centroidArrow);
+// scene.add(rolloffArrow);
+// scene.add(rmsArrow);
 
-// methods
+const resizeObserver = new ResizeObserver(resizeCanvasToDisplaySize);
+resizeObserver.observe(canvas, { box: "content-box" })
 
-const resizeIfNeed = () => {
-  const canvas = renderer.domElement;
-  let w = Math.floor(canvas.clientWidth * pixelRatio);
-  let h = Math.floor(w / aspectRatio);
-  const needResize = canvas.width !== w || canvas.height !== h;
-  if (needResize) {
-    renderer.setSize(w, h, false);
-    composer.setSize(w, h);
-    camera.aspect = aspectRatio;
-    camera.updateProjectionMatrix();
-  }
-}
-
-const render = () => {
-  //
-  resizeIfNeed();
-
-  const time = performance.now();
-
-  if (audioFeaturesExtractor) {
-    const features = audioFeaturesExtractor.features();
-    const signal = audioFeaturesExtractor.signal();
-
-    if (features) {
-      // fill ffts spectrum buffers
-      ffts.pop();
-      ffts.unshift(features.amplitudeSpectrum);
-      // 
-      const loudness = invlerp(7, 40, features.loudness.total)
-      const rms = features.rms
-      const sharpness = features.perceptualSharpness //invlerp(0.7, 4, features.perceptualSharpness)
-      // affect blooming with sharpness and loudness 
-      bloomPass.strength = lerp(1, 1.25, loudness)
-      bloomPass.radius = lerp(1, 2.5, sharpness)
-
-      // render ffts
-      for (let i = 0; i < ffts.length; i++) {
-        const geom = fftMeshes.children[i].geometry
-        for (let j = 0; j < ffts[i].length * 3; j++) {
-          let index = j * 3;
-          geom.attributes.position.array[index + 0] = +40.0 + 40 * Math.log10(j / ffts[i].length);
-          geom.attributes.position.array[index + 1] = -10. + 0.5 + sharpness + 0.5 + (ffts[i][j]) * (1.5 * loudness + rms);
-          geom.attributes.position.array[index + 2] = +15 - i * 1.1
-        }
-        geom.attributes.position.needsUpdate = true;
-      }
-
-      // render Spectral Centroid arrow
-      if (features.spectralCentroid) {
-        centroidArrow.position.set(
-          12 + 8 * Math.log10(features.spectralCentroid / (bufferSize / 2)),
-          -6,
-          -15
-        );
-      }
-
-      // render spectral rolloff arrow
-      if (features.spectralRolloff) {
-        let rolloff = features.spectralRolloff / 22050;
-        rolloffArrow.position.set(
-          12 + 8 * Math.log10(rolloff),
-          -6,
-          -15
-        );
-      }
-
-      // render rms arrow
-      if (features.rms) {
-        rmsArrow.position.set(
-          -11,
-          -5 + 10 * features.rms,
-          -15
-        );
-      }
-
-      /* const bl = bufferLine.geometry.attributes.position
-      if (!!signal) {
-        for (let i = 0; i < bufferSize; i++) {
-          let index = i * 3
-          bl.array[index] = -11 + (22 * i) / bufferSize;
-          bl.array[index + 1] = 4 + signal[i] * 5;
-          bl.array[index + 2] = -25;
-        }
-        bl.needsUpdate = true;
-      }
-   */
-      if (!!signal && imeshSignal) {
-        for (let i = 0; i < bufferSize; i++) {
-          dummy.position.set(
-            (15 * i) / bufferSize,
-            signal[i] * 10,
-            0
-          );
-          dummy.rotation.set(
-            (time + loudness) * 1e-3,
-            0,
-            loudness
-          )
-          dummy.scale.set(
-            sharpness * 0.2,
-            sharpness * 0.2,
-            sharpness * 0.2
-          )
-          dummy.updateMatrix();
-          color.set(signalPalette[i])
-          imeshSignal.setColorAt(i, color)
-          imeshSignal.setMatrixAt(i, dummy.matrix);
-        }
-        imeshSignal.instanceMatrix.needsUpdate = true
-        imeshSignal.instanceColor.needsUpdate = true
-      }
-    }
-  }
-  //
-  controls.update()
-  //
-  composer.render();
-  // 
-  stats.update()
-}
+const audioFeaturesExtractor = new AudioFeaturesExtractor();
 
 const tl = gsap.timeline()
 tl.to("#cover", {
@@ -365,7 +244,9 @@ const pause$ = fromEvent(document, "keydown")
     scan(prev => !prev)
   )
 
-const meyda$ = audioFeaturesExtractor.start()
+// observables
+
+const meyda$ = audioFeaturesExtractor.meyda$({ bufferSize })
 
 const $start = fromEvent(btn, "click")
   .pipe(
@@ -380,9 +261,7 @@ const render$ = animationFrames()
     filter(arr => !arr[1])
   )
 
-combineLatest([
-  $start, meyda$, render$
-])
+combineLatest([$start, meyda$, render$])
   .subscribe(() => {
     tl
       .to(btn, {
@@ -399,6 +278,143 @@ combineLatest([
 
     render()
   })
+
+// functions
+
+function resizeCanvasToDisplaySize() {
+
+  const { clientWidth, clientHeight, width, height } = renderer.domElement;
+
+  const pixelRatio = Math.min(1.0, window.devicePixelRatio || 1);
+
+  let w = Math.floor((clientWidth) * pixelRatio | 0);
+  let h = Math.floor((clientHeight) * pixelRatio | 0);
+
+  const needResize = width !== w || height !== h;
+  if (needResize) {
+    renderer.setSize(w, h, false);
+
+    camera.aspect = w / h;
+    camera.updateProjectionMatrix();
+
+    composer.setSize(w, h);
+
+    // target rendeer stuff
+    //postfx.resize();
+  }
+}
+
+function render() {
+
+  if (!audioFeaturesExtractor) return
+
+  const features = audioFeaturesExtractor.features();
+
+  if (!features) return
+
+  const time = performance.now();
+
+  const signal = audioFeaturesExtractor.signal();
+
+  // fill ffts spectrum buffers
+  ffts.pop();
+  ffts.unshift(features.amplitudeSpectrum);
+
+  const loudness = invlerp(7, 40, features.loudness.total)
+  const rms = features.rms
+  const sharpness = features.perceptualSharpness //invlerp(0.7, 4, features.perceptualSharpness)
+
+  // affect blooming with sharpness and loudness 
+  bloomPass.strength = lerp(1, 1.25, loudness)
+  bloomPass.radius = lerp(1, 2.5, sharpness)
+
+  // render ffts
+  for (let i = 0; i < ffts.length; i++) {
+    const geom = fftMeshes.children[i].geometry
+    const position = geom.getAttribute("position")
+    //console.log(geom.attributes.position.count)
+    for (let j = 0; j < position.count * 3; j++) {
+      let index = j * 3;
+      position.array[index + 0] = +40.0 + 40 * Math.log10(j / ffts[i].length);
+      // eslint-disable-next-line no-loss-of-precision
+      position.array[index + 1] = -10. + 0.5 + sharpness + 0.5 + (ffts[i][j]) * (1.5 * loudness + rms);
+      position.array[index + 2] = +15 - i * 1.1
+    }
+    position.needsUpdate = true;
+  }
+
+  // render Spectral Centroid arrow
+  /*  if (features.spectralCentroid) {
+     centroidArrow.position.set(
+       12 + 8 * Math.log10(features.spectralCentroid / (bufferSize / 2)),
+       -6,
+       -15
+     );
+   } */
+
+  // render spectral rolloff arrow
+  /* if (features.spectralRolloff) {
+    let rolloff = features.spectralRolloff / 22050;
+    rolloffArrow.position.set(
+      12 + 8 * Math.log10(rolloff),
+      -6,
+      -15
+    );
+  } */
+
+  // render rms arrow
+  /* if (features.rms) {
+    rmsArrow.position.set(
+      -11,
+      -5 + 10 * features.rms,
+      -15
+    );
+  } */
+
+  /* const bl = bufferLine.geometry.attributes.position
+  if (!!signal) {
+    for (let i = 0; i < bufferSize; i++) {
+      let index = i * 3
+      bl.array[index] = -11 + (22 * i) / bufferSize;
+      bl.array[index + 1] = 4 + signal[i] * 5;
+      bl.array[index + 2] = -25;
+    }
+    bl.needsUpdate = true;
+  }
+*/
+  // domain-time spectrum via instancedgeometry
+  if (!!signal && imeshSignal) {
+    for (let i = 0; i < bufferSize; i++) {
+      dummy.position.set(
+        (15 * i) / bufferSize,
+        signal[i] * 10,
+        0
+      );
+      dummy.rotation.set(
+        (time + loudness) * 1e-3,
+        0,
+        loudness
+      )
+      dummy.scale.set(
+        sharpness * 0.2,
+        sharpness * 0.2,
+        sharpness * 0.2
+      )
+      dummy.updateMatrix();
+      color.set(signalPalette[i])
+      imeshSignal.setColorAt(i, color)
+      imeshSignal.setMatrixAt(i, dummy.matrix);
+    }
+    imeshSignal.instanceMatrix.needsUpdate = true
+    imeshSignal.instanceColor.needsUpdate = true
+  }
+
+  controls.update()
+
+  composer.render();
+
+  stats.update()
+}
 
 // Mouse event stream emits on mousemove
 /* const mouse$ = fromEvent(canvas, 'mousemove')
